5aa540ff9fb2a85cb2056927c8a41a13a59b8664
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2017-01-20 17:04:57 +0530

    BASIC CRUD operaions and DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ticket.create(created_by: "User One", description: "Ticket One", severity: 1)
+Ticket.create(created_by: "User Two", description: "Ticket Two", severity: 2)
+Ticket.create(created_by: "User Three", description: "Ticket Three", severity: 3)
